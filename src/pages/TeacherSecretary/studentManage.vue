077c86eaed57c8fedee95dead57d3ce57c75f45b
songjnming <31236015+bangsjm@users.noreply.github.com>
2018-12-09 09:58:28 +0800

    学生管理

--- src/pages/TeacherSecretary/studentManage.vue
+++ src/pages/TeacherSecretary/studentManage.vue
@@ -5,19 +5,19 @@
        <template slot="header">
          <el-row :gutter="10">
             <el-col :md="3">
-              <el-select v-model="selectcollege" placeholder="请选择学院" @focus="getCollege()">
+              <el-select v-model="selectcollege" placeholder="请选择学院" @focus="getCollege()" no-data-text="暂无学院">
                 <el-option v-for="(v, k) in college" :key="k" :label="v.collegeName" :value="v.collegeNumber"></el-option>
               </el-select>
             </el-col>
             <el-col :md="3">
-              <el-select v-model="selectmajor" placeholder="请选择专业" @focus="getMajor()">
-                <el-option v-for="(v, k) in majors" :key="k" :label="v.majorName" :value="v.majorName"></el-option>
+              <el-select v-model="selectmajor" placeholder="请选择专业" @focus="getMajor()" no-data-text="暂无专业">
+                <el-option v-for="(v, k) in majors" :key="k" :label="v.majorName" :value="v.majorNumber"></el-option>
               </el-select>
             </el-col>
 
             <el-col :md="3">
-              <el-select v-model="selectclass" placeholder="请选择班级" @focus="getClass()">
-                <el-option v-for="(v, k) in classes" :key="k" :label="v.className" :value="v.classId"></el-option>
+              <el-select v-model="selectclass" placeholder="请选择班级" @focus="getClass()" no-data-text="暂无班级">
+                <el-option v-for="(v, k) in classes" :key="k" :label="v.className" :value="v.className"></el-option>
               </el-select>
             </el-col>
 
@@ -43,24 +43,18 @@
        <template slot="columns">
           <el-table-column type="selection" width="45">
           </el-table-column>
-          <el-table-column type="index" :index="(pageNum - 1) * pageSize + 1">
+          <el-table-column prop="studentNumber" label="学号">
           </el-table-column>
-          <el-table-column prop="droleName" label="学生姓名">
+          <el-table-column prop="studentName" label="学生姓名">
+          </el-table-column>
+          <el-table-column prop="sex" label="性别">
+          </el-table-column>
+          <el-table-column prop="email" label="邮箱">
+          </el-table-column>
+          <el-table-column label="操作">
             <template slot-scope="scope">
-              <el-button type="text" size="small" @click="showEdit(scope.row)">{{ scope.row.droleName }}</el-button>
-            </template>
-          </el-table-column>
-          <el-table-column prop="droleDesc" label="描述">
-          </el-table-column>
-          <el-table-column prop="status" label="状态">
-            <template slot-scope="scope">
-              <span>{{dict('status',scope.row.status)}}</span>
-            </template>
-          </el-table-column>
-          <el-table-column label="权限">
-            <template slot-scope="scope">
-              <el-button type="text" size="small" @click="showUpdate(scope.row.droleId,scope.row.droleName)">权限
-              </el-button>
+              <el-button type="text" size="small" @click="showEdit(scope.row)">编辑</el-button>
+              <el-button type="text" size="small" @click="showEditPwd(scope.row)">修改密码</el-button>
             </template>
           </el-table-column>
         </template>
@@ -71,11 +65,50 @@
         </template>
      </Table2>
     </el-card>
+
+    <el-dialog title="修改密码"
+               custom-class="m1-dialog el-col-md-10 el-col-md-push-7 el-col-sm-14 el-col-sm-push-5 el-col-xs-22 el-col-xs-push-1"
+               :visible.sync="isShowPwdDialog">
+      <el-form :model="form" :rules="rules" ref="pwdForm" label-width="80px">
+        <el-form-item label="新密码" prop="password">
+          <el-input type="password" v-model="form.password" placeholder="请输入新密码"></el-input>
+        </el-form-item>
+        <el-form-item label="确认密码" prop="confirmPassword">
+          <el-input type="password" v-model="form.confirmPassword" placeholder="请再次输入新密码"></el-input>
+        </el-form-item>
+        <el-form-item label="" label-width="0" class="dialog-btn-group">
+          <el-button type="success" size="small" @click="handleEditPwd">保存</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
+
+      <el-dialog title="编辑"
+              custom-class="m1-dialog el-col-md-10 el-col-md-push-7 el-col-sm-14 el-col-sm-push-5 el-col-xs-22 el-col-xs-push-1"
+              :visible.sync="isShowEditDialog">
+        <el-form :model="form" :rules="rules" ref="form" label-width="80px">
+          <el-form-item label="学号" prop="studentNumber">
+            <el-input v-model="form.studentNumber" placeholder="请输入学号"></el-input>
+          </el-form-item>
+          <el-form-item label="学生姓名" prop="studentName">
+            <el-input v-model="form.studentName" disabled="disabled" placeholder="请输入学生姓名"></el-input>
+          </el-form-item>
+          <el-form-item label="性别 " prop="sex">
+            <el-input v-model="form.sex" disabled="disabled" placeholder="请选择学生性别"></el-input>
+          </el-form-item>
+          <el-form-item label="邮箱" prop="email">
+            <el-input v-model="form.email" placeholder="请输入邮箱"></el-input>
+          </el-form-item>
+          <el-form-item label="" label-width="0" class="dialog-btn-group">
+            <el-button type="success" size="small" @click="handleSave">保存</el-button>
+          </el-form-item>
+        </el-form>
+    </el-dialog>
   </div>
 </template>
 
 <script>
   import Table2 from "@/components/table/Table1";
+  import {email} from '@/assets/js/common/validate'
 
   export default {
     name: "studentManage",
@@ -83,6 +116,16 @@
       Table2
     },
     data() {
+      
+      var passwordValidate = (rule, value, callback) => {
+        if (value === "") {
+          callback(new Error("请再次输入密码"));
+        } else if (value !== this.form.password) {
+          callback(new Error("两次输入的密码不一致"));
+        } else {
+          callback();
+        }
+      };
       return {
         selectcollege:"",
         selectmajor:"",
@@ -91,6 +134,39 @@
         majors:[],
         classes:[],
         uploadURL:"",
+        tableData: [],
+        pageNum: 1,
+        pageSize: 10,
+        total: 0,
+        handlePage: 1,
+        searchNumber:"",
+        isShowPwdDialog:false,
+        isShowEditDialog:false,
+        form: {
+          studentName: "",
+          studentNumber: "",
+          oldstudentNumber:"",
+          sex: "",
+          email: "",
+          password:"",
+          confirmPassword:"",
+        },
+        rules: {
+          password: [
+            {required: true, message: "请输入密码", trigger: "blur"},
+            {max: 100, message: "最大长度为 100 个字符", trigger: "blur"}
+          ],
+          confirmPassword: [
+            {required: true, message: "请再次输入密码", trigger: "blur"},
+            {max: 100, message: "最大长度为 100 个字符", trigger: "blur"},
+            {validator: passwordValidate, trigger: "blur"}
+          ],
+          email:[
+            {required: true, message: "请输入邮箱", trigger: "blur"},
+            {max: 50, message: "最大长度为 50 个字符", trigger: "blur"},
+            {pattern: email, message: "邮箱格式不正确", trigger: "blur"}
+          ],
+        },
       };
     },
     created() {
@@ -104,13 +180,91 @@
       });
     },
     methods: {
+      
+        handleSizeChange(pageSize) {
+          this.pageSize = pageSize;
+          this.handleSearch();
+        },
+        handleCurrentChange(pageNum) {
+          this.pageNum = pageNum;
+          this.handleSearch();
+        },
         getParams () {
           this.token = window.sessionStorage.getItem("token");
           this.$nextTick(()=>{
             this.uploadURL = this.$http.defaults.baseURL + "/TeacherSecretary/excelAddStudent?token="+this.token;
           });
         },
+        
+        showEdit(row) {
+            this.form.studentName=row.studentName;
+            this.form.oldStudentNumber=row.studentNumber;
+            this.form.studentNumber=row.studentNumber;
+            this.form.sex=row.sex;
+            this.form.email=row.email;
+            this.isShowEditDialog = true;
+        },
+        handleEditPwd() {
+          this.$refs["pwdForm"].validate((pass, o) => {
+            if (pass) {
+              this.isLoading = true;
+              let formData = new FormData()
+              formData.append('studentNumber', this.form.studentNumber)
+              formData.append('password', this.form.password)
+              this.$http.post("/TeacherSecretary/resetPassword", formData, {
+                hideLoading: true,
+              }).then(res => {
+                this.isLoading = false
+                let body = res.data;
+                if (body.code === "200") {
+                  this.$message({
+                    message: "保存成功",
+                    type: "success"
+                  });
+                  this.isShowPwdDialog = false;
+                  this.handleSearch();
+                } else {
+                  this.$message.error(body.msg);
+                }
+              });
+            } else {
+              this.$message.error("表单输入不正确");
+            }
+          });
+        },
+        showEditPwd(row) {
+          this.form.studentNumber = row.studentNumber;
+          this.isShowPwdDialog = true;
+          this.$nextTick(() => {
+            this.$refs["pwdForm"].clearValidate();
+          });
+        },
+        
+        handleSave() {
+          this.$refs["form"].validate((pass, o) => {
+            if (pass) {
+              this.$http.put("/TeacherSecretary/resetInformation", this.form).then(res => {
+                let body = res.data;
+                if (body.code === "200") {
+                  this.$message({
+                    message: "保存成功",
+                    type: "success"
+                  });
+                  this.isShowEditDialog = false;
+                  this.handleSearch();
+                } else {
+                  this.$message.error(body.msg);
+                }
+              });
+            } else {
+              this.$message.error("表单输入不正确");
+            }
+          });
+        },
         getCollege(){
+          this.selectcollege="";
+          this.selectmajor="";
+          this.selectclass="";
           this.college=[];
           this.$http
           .post("/TeacherSecretary/getCollege","",{
@@ -126,6 +280,8 @@
           });
         },
         getMajor(){
+          this.selectmajor="";
+          this.selectclass="";
           this.majors = [];
           let formData = new FormData()
           formData.append('collegeNumber', this.selectcollege)
@@ -145,6 +301,7 @@
           )
         },
         getClass(){
+          this.selectclass="";
           this.classes = [];
           let formData = new FormData()
           formData.append('majorName',this.selectmajor)
@@ -174,6 +331,44 @@
         handleSuccess(){
           this.$message.success(`上传成功`);
           this.$refs.upload.clearFiles();
+        },
+        
+        handleSearch() {
+          var selectclass=this.selectclass;
+          var year=selectclass.substring(selectclass.length-3,selectclass.length-1);
+          var classNumber='0'+selectclass.charAt(selectclass.length-1);
+          this.searchNumber=year+this.selectcollege+this.selectmajor+classNumber;
+          this.$http
+            .get("/TeacherSecretary/search", {
+              params: {
+                searchNumber:this.searchNumber,
+                pageNum: this.pageNum,
+                pageSize: this.pageSize
+              }
+            })
+          .then(res => {
+            let body = res.data;
+            if (body.code === "200") {
+              this.tableData = body.data;
+              this.total = body.page.total;
+              this.pageSize = body.page.size;
+              this.handlePage = this.pageNum;
+              let result = this.total / this.pageSize;
+              if (Math.floor(result) != result) {
+                result++;
+              }
+
+              if (this.pageNum > result) {
+                this.pageNum = Math.floor(result);
+                if (this.pageNum == 0) {
+                  this.pageNum = 1;
+                  return
+                }
+                this.handleSearch();
+              }
+
+              }
+            });
         },
       }
   }
